d2ac29b2c91bf26a45628fe6d18beb4baca4deec
unknown <seeweiyang1@gmail.com>
2021-06-03 04:15:09 +0800

    first commit

--- src/components/Workshops/Workshops.js
+++ src/components/Workshops/Workshops.js
@@ -2,8 +2,53 @@ import styles from "./Workshops.module.css";
 
 const Workshops = () => {
   return (
-    <div className={styles.workshops} name="workshops">
-      this is the workshops section
+    <div className={styles.workshops} name="prizes">
+      <h1>Available Workshops</h1>
+      <p>
+        Insert message
+        <br />
+      </p>
+
+      <div className={styles.row}>
+        <div className={styles.descriptionCol}>
+          <h3>Intro to HTML/CSS</h3>
+          <p>Timing: placeholder Description: placeholder</p>
+        </div>
+        <div className={styles.descriptionCol}>
+          <h3>Intro to HTML/CSS</h3>
+          <p>Timing: placeholder Description: placeholder</p>
+        </div>
+
+        <div className={styles.descriptionCol}>
+          <h3>Intro to JavaScript</h3>
+          <p>Timing: placeholder Description: placeholder</p>
+        </div>
+
+        <div className={styles.descriptionCol}>
+          <h3>Intro to ReactJS</h3>
+          <p>Timing: placeholder Description: placeholder</p>
+        </div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.descriptionCol}>
+          <h3>Intro to HTML/CSS</h3>
+          <p>Timing: placeholder Description: placeholder</p>
+        </div>
+
+        <div className={styles.descriptionCol}>
+          <h3>Intro to JavaScript</h3>
+          <p>Timing: placeholder Description: placeholder</p>
+        </div>
+
+        <div className={styles.descriptionCol}>
+          <h3>Intro to ReactJS</h3>
+          <p>Timing: placeholder Description: placeholder</p>
+        </div>
+        <div className={styles.descriptionCol}>
+          <h3>Intro to JavaScript</h3>
+          <p>Timing: placeholder Description: placeholder</p>
+        </div>
+      </div>
     </div>
   );
 };
