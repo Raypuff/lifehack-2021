67f50598f4582abc202fde55f5f9cafb34ce3173
unknown <seeweiyang1@gmail.com>
2021-06-03 04:08:02 +0800

    Revert "first commit"
    This reverts commit a3fee586786e8d73081fa7e3a44f5b21f70c1ed7.

--- src/components/Workshops/Workshops.js
+++ src/components/Workshops/Workshops.js
@@ -2,53 +2,8 @@ import styles from "./Workshops.module.css";
 
 const Workshops = () => {
   return (
-    <div className={styles.workshops} name="prizes">
-      <h1>Available Workshops</h1>
-      <p>
-        Insert message
-        <br />
-      </p>
-
-      <div className={styles.row}>
-        <div className={styles.descriptionCol}>
-          <h3>Intro to HTML/CSS</h3>
-          <p>Timing: placeholder Description: placeholder</p>
-        </div>
-        <div className={styles.descriptionCol}>
-          <h3>Intro to HTML/CSS</h3>
-          <p>Timing: placeholder Description: placeholder</p>
-        </div>
-
-        <div className={styles.descriptionCol}>
-          <h3>Intro to JavaScript</h3>
-          <p>Timing: placeholder Description: placeholder</p>
-        </div>
-
-        <div className={styles.descriptionCol}>
-          <h3>Intro to ReactJS</h3>
-          <p>Timing: placeholder Description: placeholder</p>
-        </div>
-      </div>
-      <div className={styles.row}>
-        <div className={styles.descriptionCol}>
-          <h3>Intro to HTML/CSS</h3>
-          <p>Timing: placeholder Description: placeholder</p>
-        </div>
-
-        <div className={styles.descriptionCol}>
-          <h3>Intro to JavaScript</h3>
-          <p>Timing: placeholder Description: placeholder</p>
-        </div>
-
-        <div className={styles.descriptionCol}>
-          <h3>Intro to ReactJS</h3>
-          <p>Timing: placeholder Description: placeholder</p>
-        </div>
-        <div className={styles.descriptionCol}>
-          <h3>Intro to JavaScript</h3>
-          <p>Timing: placeholder Description: placeholder</p>
-        </div>
-      </div>
+    <div className={styles.workshops} name="workshops">
+      this is the workshops section
     </div>
   );
 };
