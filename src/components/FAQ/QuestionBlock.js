f622f8617b85186b728021564920ab4c9a6161f9
laitzheng <laitzheng@gmail.com>
2021-06-04 15:05:29 +0800

    Create FAQ section

--- src/components/FAQ/QuestionBlock.js
+++ src/components/FAQ/QuestionBlock.js
@@ -3,7 +3,7 @@ import styles from "./FAQ.module.css";
 
 export default function QuestionBlock(props) {
 	return (
-		<div className={styles.questionBlock}>
+		<div className={styles.questionItem}>
 			<h3>{props.question}</h3>
 			<p>{props.answer}</p>
 		</div>
