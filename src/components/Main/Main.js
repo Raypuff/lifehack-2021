16db14cc6f24f9aab14bee8d7650012b343e7c61
Rui En <neoruien123@gmail.com>
2021-06-07 16:36:22 +0800

    Add responsive design

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,12 +3,10 @@ import styles from "./Main.module.css";
 const Main = () => {
   return (
     <div name="main" className={styles.mainBody}>
-      <div className={styles.myimage}>
-        <img src="LifeHack 1500x1500 logo.png" alt="LifeHack logo"></img>
-      </div>
+      <img className={styles.myimage} src="LifeHack 1500x1500 logo.png" alt="LifeHack logo"></img>
       <div className={styles.description}>
-        <h>LifeHack 2021</h>
-        <p>LifeHack 2021 is a 24h virtual hackathon that wants YOU to develop innovative software solutions that can make a change in peoples’ lives in a post-COVID world.</p>
+        <h className={styles.title}>LifeHack 2021</h>
+        <p className={styles.subtitle}>LifeHack 2021 is a 24h virtual hackathon that wants YOU to develop innovative software solutions that can make a change in peoples’ lives in a post-COVID world.</p>
       </div>
     </div>
   );
