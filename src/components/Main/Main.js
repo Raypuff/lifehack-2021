16ec54ab0b49df9ecad1883793954c78aec6c5fd
peilin <peilinye18@gmail.com>
2021-06-07 15:50:41 +0800

    main page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,7 +3,13 @@ import styles from "./Main.module.css";
 const Main = () => {
   return (
     <div name="main" className={styles.mainBody}>
-      this is main content
+      <div className={styles.myimage}>
+        <img src="LifeHack 1500x1500 logo.png" alt="LifeHack logo"></img>
+      </div>
+      <div className={styles.description}>
+        <h>LifeHack 2021</h>
+        <p>LifeHack 2021 is a 24h virtual hackathon that wants YOU to develop innovative software solutions that can make a change in peoples’ lives in a post-COVID world.</p>
+      </div>
     </div>
   );
 };
