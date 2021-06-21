4f71b650fc1a94095d2f820df5dfedd42e7b558e
Rayner <raynerljm@gmail.com>
2021-06-21 13:06:05 +0800

    Make navbar responsive
    Make register now button look good and centralise footer.

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -1,5 +1,7 @@
 import styles from "./Sponsors.module.css";
 import sponsorLogo from "../../images/sponsor-logo.png";
+import micronLogo from "../../images/micron-logo.png";
+import csitLogo from "../../images/csit-logo.png";
 import socLogo from "../../images/soc-logo.png";
 import compClubLogo from "../../images/comp_club-logo.png";
 
@@ -12,22 +14,26 @@ import compClubLogo from "../../images/comp_club-logo.png";
 // };
 
 const Sponsors = () => {
-  return (
-    <div className={styles.sponsors} name="sponsors">
-      <h2>Sponsors and Organizers</h2>
-      <p className={styles.title}>Sponsored By</p>
-      <div className={styles.logoContainer}>
-        <img className={styles.logo} src={sponsorLogo} alt="Placeholder"/>
-        <img className={styles.logo} src={sponsorLogo} alt="Placeholder"/>
-        <img className={styles.logo} src={sponsorLogo} alt="Placeholder"/>
-      </div>
-      <p className={styles.title}>Organized By</p>
-      <div className={styles.logoContainer}>
-        <img className={styles.logo} src={socLogo} alt="NUS Computing"/>
-        <img className={styles.logo} src={compClubLogo} alt="NUS Students' Computing Club"/>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.sponsors} name="sponsors">
+			<h2 className={styles.mainTitle}>Sponsors and Organizers</h2>
+			<p className={styles.title}>Sponsored By</p>
+			<div className={styles.logoContainer}>
+				<img className={styles.logo} src={micronLogo} alt="Placeholder" />
+				<img className={styles.logo} src={csitLogo} alt="Placeholder" />
+				<img className={styles.logo} src={sponsorLogo} alt="Placeholder" />
+			</div>
+			<p className={styles.title}>Organized By</p>
+			<div className={styles.logoContainer}>
+				<img className={styles.logo} src={socLogo} alt="NUS Computing" />
+				<img
+					className={styles.logo}
+					src={compClubLogo}
+					alt="NUS Students' Computing Club"
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Sponsors;
