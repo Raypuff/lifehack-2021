7081b19d5a4daa1e4b211237713844a1edde0880
Rayner <raynerljm@gmail.com>
2021-06-22 23:04:23 +0800

    Add new sponsors, tweak the main section for mobile view and tweaked the FAQ

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -16,21 +16,17 @@ const FAQ = () => {
 			qn: "How will the submissions be assessed?",
 			ans: "Details on judging criteria and rules will be elaborated upon on the event Opening Day (23 July). Judging panels will consist of NUS professors and industry professionals.",
 		},
-		{
-			qn: "Must I be present for the zoom session if my team is shortlisted?",
-			ans: "At least one member of the team has to be present. If no members are available, the team will be disqualified.",
-		},
-		{
+				{
 			qn: "Is the hackathon fully online?",
 			ans: "Yes. LifeHack 2021 will be a fully online event from 19 to 25 July 2021.",
 		},
 		{
 			qn: "Do I need any technical experience or knowledge?",
-			ans: "Basic programming competencies are expected, else all are welcome! We have workshops planned for you over 19 to 22 July. Attending the workshops will provide you with resources to conceptualize and create your project.",
+			ans: "Some knowledge of programming is recommended but not necessary! We have workshops planned for you over 19 to 22 July. Attending the workshops will provide you with resources and ideas to help you conceptualize and create your hack.",
 		},
 		{
 			qn: "Can I use code I previously wrote?",
-			ans: "You are allowed to use previously written code. However, the copying of ideas and code is not allowed.",
+			ans: "You are allowed to use snippets of code you have previously written. However, copying your entire hack from previous events or from online is strictly not allowed.",
 		},
 		{
 			qn: "Are there any restrictions on platforms and programming languages which can be used?",
@@ -39,29 +35,19 @@ const FAQ = () => {
 	];
 
 	return (
+		<div className={styles.background}>
 		<div className={styles.faq} name="faq">
 			<h2>FAQ</h2>
 			<div className={styles.questionBox}>
 				{questionList.map((questionItem) => (
 					<QuestionBlock question={questionItem.qn} answer={questionItem.ans} />
 				))}
-				<div className={styles.questionItem}>
-					<h3>Who owns the intellectual property rights to my submission?</h3>
-					<p>
-						For IP Rights agreement, please refer to this <a href=" / ">document</a>.
-					</p>
-				</div>
-				<div className={styles.questionItem}>
-					<h3>Where can I get the latest updates and announcements?</h3>
-					<p>
-						Join us in our Telegram channel <a href="/">here</a> for the latest updates!
-					</p>
-				</div>
-			</div>
+											</div>
 			<div className={styles.furtherEnq}>
 				<h5>For futher enquries, kindly email lifehack@nuscomputing.com</h5>
 			</div>
 		</div>
+</div>
 	);
 };
 
