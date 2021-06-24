ac69db48e035475edb697b113b037a9f6c27b3c5
Rui En <neoruien123@gmail.com>
2021-06-24 12:13:30 +0800

    Finish making header responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,66 +1,72 @@
 import { useState } from "react";
 import HeaderItem from "./HeaderItem";
-import logo from "../../images/lifehack-logo.png";
+import logo from "../../images/lifehack-logo.svg";
 import styles from "./Header.module.css";
 
 const Header = () => {
-	const [navActive, setNavActive] = useState(false);
+  const [navActive, setNavActive] = useState(false);
 
-	const toggleNav = () => {
-		setNavActive(!navActive);
-	}
+  const toggleNav = () => {
+    setNavActive(!navActive);
+  };
 
-	const closeNav = () => {
-		setNavActive(false);
-	}
+  const closeNav = () => {
+    setNavActive(false);
+  };
 
-	return (
-		<nav className={styles.nav}>
-			<div className={styles.logoContainer}>
-				<HeaderItem to="main" onClick={closeNav} content={
-					<img
-						className={styles.logo}
-						title="LifeHack 2021"
-						src={logo}
-						alt="LifeHack Logo"
-					/>
-				}/>
-			</div>
-			<ul className={navActive ? styles.navActive : styles.navLinks}>
-				<li>
-					<HeaderItem to="timeline" onClick={closeNav} content="Timeline" />
-				</li>
-				<li>
-					<HeaderItem to="workshops" onClick={closeNav} content="Workshops" />
-				</li>
-				<li>
-					<HeaderItem to="prizes" onClick={closeNav} content="Prizes" />
-				</li>
-				<li>
-					<HeaderItem to="faq" onClick={closeNav} content="FAQ" />
-				</li>
-				<li>
-					<HeaderItem to="sponsors" onClick={closeNav} content="Sponsors" />
-				</li>
-				<li>
-					<a
-						href="https://www.instagram.com/nuscomputingclub/"
-						target="_blank"
-						rel="noreferrer"
-						className={styles.registerButton}
-						onClick={closeNav}
-					>
-						REGISTER NOW
-					</a>
-				</li>
-			</ul>
-			<div className={styles.burger} onClick={toggleNav}>
-				<div className={navActive ? styles.line1 : styles.inactive}></div>
-				<div className={navActive ? styles.line2 : styles.inactive}></div>
-				<div className={navActive ? styles.line3 : styles.inactive}></div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.logoContainer}>
+        <HeaderItem
+          to="main"
+          onClick={closeNav}
+          content={
+            <img
+              className={styles.logo}
+              title="LifeHack 2021"
+              src={logo}
+              alt="LifeHack Logo"
+            />
+          }
+        />
+      </div>
+      <ul className={navActive ? styles.navActive : styles.navLinks}>
+        <li>
+          <HeaderItem to="timeline" onClick={closeNav} content="Timeline" />
+        </li>
+        <li>
+          <HeaderItem to="workshops" onClick={closeNav} content="Workshops" />
+        </li>
+        <li>
+          <HeaderItem to="prizes" onClick={closeNav} content="Prizes" />
+        </li>
+        <li>
+          <HeaderItem to="faq" onClick={closeNav} content="FAQ" />
+        </li>
+        <li>
+          <HeaderItem to="sponsors" onClick={closeNav} content="Sponsors" />
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/nuscomputingclub/"
+            target="_blank"
+            rel="noreferrer"
+            className={styles.registerButton}
+            onClick={closeNav}
+          >
+            REGISTER NOW
+          </a>
+        </li>
+      </ul>
+      <div className={styles.burgerContainer}>
+        <div className={styles.burger} onClick={toggleNav}>
+          <div className={navActive ? styles.line1 : styles.inactive}></div>
+          <div className={navActive ? styles.line2 : styles.inactive}></div>
+          <div className={navActive ? styles.line3 : styles.inactive}></div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
