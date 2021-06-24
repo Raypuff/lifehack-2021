93e31fc1923fb6d2faa46a691b19da8340e1bb5f
Rui En <neoruien123@gmail.com>
2021-06-24 17:12:21 +0800

    Add slide animation to Main description

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Bounce from "react-reveal/Bounce";
+import Slide from "react-reveal/Slide";
 import logo from "../../images/lifehack-logo.png";
 import styles from "./Main.module.css";
 
@@ -11,21 +12,23 @@ const Main = () => {
           <img className={styles.logo} src={logo} alt="LifeHack logo"></img>
         </div>
       </Bounce>
-      <div className={styles.description}>
-        <p className={styles.title}>LifeHack 2021</p>
-        <p className={styles.date}>23-24 July</p>
-        <p className={styles.subtitle}>
-          Stay ahead of change. Innovate the future
-        </p>
-        <a
-          href="https://www.instagram.com/nuscomputingclub/"
-          target="_blank"
-          rel="noreferrer"
-          className={styles.registerButton}
-        >
-          REGISTER NOW
-        </a>
-      </div>
+      <Slide right cascade>
+        <div className={styles.description}>
+          <p className={styles.title}>LifeHack 2021</p>
+          <p className={styles.date}>23-24 July</p>
+          <p className={styles.subtitle}>
+            Stay ahead of change. Innovate the future
+          </p>
+          <a
+            href="https://www.instagram.com/nuscomputingclub/"
+            target="_blank"
+            rel="noreferrer"
+            className={styles.registerButton}
+          >
+            REGISTER NOW
+          </a>
+        </div>
+      </Slide>
       <div className={styles.description2}>
         <p className={styles.date}>23-24 July 2021</p>
         <p className={styles.subtitle}>
