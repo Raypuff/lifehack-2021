4c31972eafad2e48f7c5381b0c5310e855b45dec
Rui En <neoruien123@gmail.com>
2021-06-24 17:19:59 +0800

    Add animations to Register page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,15 +1,21 @@
+import Fade from "react-reveal/Fade";
+import Tada from "react-reveal/Tada";
 import styles from "./Register.module.css";
 
 const Register = () => {
-	return (
-		<div className={styles.register} name="register">
-			<p className={styles.text}>
-				So what are you waiting for?
-				<br />
-			</p>
-			<button className={styles.registerButton}>REGISTER NOW</button>
-		</div>
-	);
+  return (
+    <div className={styles.register} name="register">
+      <Fade>
+        <p className={styles.text}>
+          So what are you waiting for?
+          <br />
+        </p>
+      </Fade>
+      <Tada>
+        <button className={styles.registerButton}>REGISTER NOW</button>
+      </Tada>
+    </div>
+  );
 };
 
 export default Register;
