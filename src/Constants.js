a4faee30aa996c3e6a2d9e261b5c33e633df1915
Rayner <raynerljm@gmail.com>
2021-06-25 16:51:10 +0800

    Add links to Sponsors

--- src/Constants.js
+++ src/Constants.js
@@ -3,12 +3,15 @@ const Links = {
   startIT: "https://www.instagram.com/nusstartit/",
   nusHackers: "https://www.nushackers.org/",
   websparks: "https://www.websparks.sg/",
-  csit: "https://www.csit.gov.sg/"
-}
+  csit: "https://www.csit.gov.sg/",
+  micron: "https://www.micron.com/",
+  websparks: "https://www.websparks.sg/",
+  shopee: "https://shopee.sg/",
+  dsta: "https://www.dsta.gov.sg/",
+  soc: "https://www.comp.nus.edu.sg/",
+  compclub: "https://nuscomputing.com/",
+};
 
-const email = "lifehack@nuscomputing.com";
+const email = "academic.liaison@nuscomputing.com";
 
-export {
-  Links,
-  email
-}
+export { Links, email };
--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -9,12 +9,12 @@ import startitLogo from "../../images/organizations/startit-logo.png";
 import nusHackersLogo from "../../images/organizations/nus-hackers.png";
 import socLogo from "../../images/organizations/soc-logo.png";
 import compClubLogo from "../../images/organizations/comp_club-logo.png";
+import { Links } from "../../Constants";
 import styles from "./Sponsors.module.css";
 
 const Sponsors = () => {
   return (
     <div className={styles.background}>
-      {" "}
       <div className={styles.sponsors} name="sponsors">
         <h2 className={styles.header}>Sponsors and Organizers</h2>
         <Fade bottom>
@@ -22,18 +22,22 @@ const Sponsors = () => {
         </Fade>
         <Fade>
           <div className={styles.logoContainer}>
-            <img
-              className={styles.logo}
-              src={micronLogo}
-              alt="Micron"
-              loading="lazy"
-            />
-            <img
-              className={styles.logo}
-              src={csitLogo}
-              alt="CSIT"
-              loading="lazy"
-            />
+            <a href={Links.micron} target="blank" rel="noreferrer">
+              <img
+                className={styles.logo}
+                src={micronLogo}
+                alt="Micron"
+                loading="lazy"
+              />
+            </a>
+            <a href={Links.csit} target="blank" rel="noreferrer">
+              <img
+                className={styles.csitLogo}
+                src={csitLogo}
+                alt="CSIT"
+                loading="lazy"
+              />
+            </a>
           </div>
         </Fade>
         <Fade bottom>
@@ -41,36 +45,46 @@ const Sponsors = () => {
         </Fade>
         <Fade>
           <div className={styles.logoContainer}>
-            <img
-              className={styles.smallLogo}
-              src={websparksLogo}
-              alt="Websparks"
-              loading="lazy"
-            />
-            <img
-              className={styles.smallLogo}
-              src={shopeeLogo}
-              alt="Shopee"
-              loading="lazy"
-            />
-            <img
-              className={styles.smallLogo}
-              src={dstaLogo}
-              alt="DSTA"
-              loading="lazy"
-            />
-            <img
-              className={styles.smallLogo}
-              src={nusHackersLogo}
-              alt="NUS Hackers"
-              loading="lazy"
-            />
-            <img
-              className={styles.smallLogo}
-              src={startitLogo}
-              alt="StartIT"
-              loading="lazy"
-            />
+            <a href={Links.websparks} target="blank" rel="noreferrer">
+              <img
+                className={styles.smallLogo}
+                src={websparksLogo}
+                alt="Websparks"
+                loading="lazy"
+              />
+            </a>
+            <a href={Links.shopee} target="blank" rel="noreferrer">
+              <img
+                className={styles.smallLogo}
+                src={shopeeLogo}
+                alt="Shopee"
+                loading="lazy"
+              />
+            </a>
+            <a href={Links.dsta} target="blank" rel="noreferrer">
+              <img
+                className={styles.smallLogo}
+                src={dstaLogo}
+                alt="DSTA"
+                loading="lazy"
+              />
+            </a>
+            <a href={Links.nusHackers} target="blank" rel="noreferrer">
+              <img
+                className={styles.smallLogo}
+                src={nusHackersLogo}
+                alt="NUS Hackers"
+                loading="lazy"
+              />
+            </a>
+            <a href={Links.startIT} target="blank" rel="noreferrer">
+              <img
+                className={styles.smallLogo}
+                src={startitLogo}
+                alt="StartIT"
+                loading="lazy"
+              />
+            </a>
           </div>
         </Fade>
         <Fade bottom>
@@ -78,18 +92,22 @@ const Sponsors = () => {
         </Fade>
         <Fade>
           <div className={styles.logoContainer}>
-            <img
-              className={styles.logo}
-              src={socLogo}
-              alt="NUS Computing"
-              loading="lazy"
-            />
-            <img
-              className={styles.logo}
-              src={compClubLogo}
-              alt="NUS Students' Computing Club"
-              loading="lazy"
-            />
+            <a href={Links.soc} target="blank" rel="noreferrer">
+              <img
+                className={styles.logo}
+                src={socLogo}
+                alt="NUS Computing"
+                loading="lazy"
+              />
+            </a>
+            <a href={Links.compclub} target="blank" rel="noreferrer">
+              <img
+                className={styles.logo}
+                src={compClubLogo}
+                alt="NUS Students' Computing Club"
+                loading="lazy"
+              />
+            </a>
           </div>
         </Fade>
       </div>
