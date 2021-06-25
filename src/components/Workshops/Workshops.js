113a4d23d74924d5ff20bf9783b540a6d324ed83
Rayner <raynerljm@gmail.com>
2021-06-25 11:22:54 +0800

    Tweak workshop wordings

--- src/components/Workshops/Workshops.js
+++ src/components/Workshops/Workshops.js
@@ -10,16 +10,16 @@ import { Links } from "../../Constants";
 const WorkshopItems = [
   {
     title: "Ultimate guide to winning your first Hackathon",
-    datetime: "19 July 4:00pm - 5:00pm",
+    date: "19 July 4:00pm - 5:00pm",
     description:
-      "So, you got an amazing hackathon idea. But that's not enough to in it! In this workshop, we show you the five essential ingredients on how to ace a hackathon",
+      "So, you got an amazing hackathon idea - But that's not enough to win it! In this workshop, we show you the five essential ingredients on how to ace a hackathon.",
     organizer: "NUS StartIT",
     organizerLogo: StartITLogo,
     organizerLink: Links.startIT,
   },
   {
     title: "How CSIT looks into AI",
-    datetime: "19 July, 7:00pm - 8:30pm",
+    date: "19 July, 7:00pm - 8:30pm",
     description:
       "As Artificial Intelligence becomes more relevant by the day, how is CSIT utilizing AI with data science?",
     organizer: "CSIT",
@@ -28,7 +28,7 @@ const WorkshopItems = [
   },
   {
     title: "Using Python to make your first Telegram bot",
-    datetime: "20 July, 7:00pm - 9:00pm",
+    date: "20 July, 7:00pm - 9:00pm",
     description:
       "Learn how you can use Python to create your very own Telegram bot!",
     organizer: "NUS Hackers",
@@ -37,16 +37,16 @@ const WorkshopItems = [
   },
   {
     title: "How to incorporate UI/UX concepts into your designs",
-    datetime: "21 July 5:00pm - 6:00pm",
+    date: "21 July 5:00pm - 6:00pm",
     description:
-      "Hear directly from industry professionals how to make designs that are not just beautiful but functional",
+      "Hear directly from industry professionals how to make designs that are not just beautiful but functional!",
     organizer: "Websparks",
     organizerLogo: WebsparksLogo,
     organizerLink: Links.websparks,
   },
   {
     title: "Web development with React",
-    datetime: "21 July 7:00pm - 10:00pm",
+    date: "21 July 7:00pm - 10:00pm",
     description:
       "Learn how you can easily use React to create your very first interactive website!",
     organizer: "Websparks",
@@ -55,9 +55,9 @@ const WorkshopItems = [
   },
   {
     title: "Operationalizing your applications at scale in the cloud!",
-    datetime: "22 July, 7:00pm - 8:30pm",
+    date: "22 July, 7:00pm - 8:30pm",
     description:
-      "Through the use of AWS cloud services, we will show you how to deploy your advanced applications such that it can handle many users",
+      "Through the use of AWS cloud services, we will show you how to deploy your advanced applications such that it can handle many users.",
     organizer: "CSIT",
     organizerLogo: CSITLogo,
     organizerLink: Links.csit,
