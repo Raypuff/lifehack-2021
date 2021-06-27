9130e196314b3327bf296866ab6b21a6846bcc43
Rui En <neoruien123@gmail.com>
2021-06-27 15:13:12 +0800

    Add react helmet for SEO

--- src/App.js
+++ src/App.js
@@ -9,11 +9,26 @@ import FAQ from "./components/FAQ";
 import Sponsors from "./components/Sponsors";
 import Register from "./components/Register";
 import Footer from "./components/Footer";
+import { Helmet } from "react-helmet";
 import styles from "./App.module.css";
 
 const App = () => {
   return (
     <div>
+      <Helmet defaultTitle="LifeHack 2021">
+        <meta
+          name="description"
+          content="LifeHack 2021 is a 24-Hour virtual hackathon that wants you to develop
+          innovative software solutions to make a positive change in peoples’
+          lives in a post-COVID world. Through exciting workshops and events,
+          you will have the chance to learn various technologies from web
+          development to utilizing cloud services."
+        />
+        <meta
+          name="keywords"
+          content="lifehack, hackathon, 2021, nus, computing, club"
+        />
+      </Helmet>
       <Header />
       <div className={styles.content}>
         <Main />
